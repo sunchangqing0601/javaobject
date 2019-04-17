97fbf1c11c5c23c480c09426ca0a25036a3d86b3
sunchangqing001 <759061715@qq.com>
2019-04-17 11:14:39 +0800

    提交hellogit2

---
+++ src/com/sun/test/HelloGit1.java
@@ -0,0 +1,10 @@
+package com.sun.test;
+
+public class HelloGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("’‚ «HelloGit1£°");
+	}
+
+}
