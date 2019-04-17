f04fc7ba850f04b4b5b24f1d1cc42c147e5318ac
sunchangqing001 <759061715@qq.com>
2019-04-17 10:47:19 +0800

    commit hellogit

---
+++ src/com/sun/test/HelloGit.java
@@ -0,0 +1,19 @@
+package com.sun.test;
+/**
+ * 
+ * @author User
+ *
+ */
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while(i<5) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
