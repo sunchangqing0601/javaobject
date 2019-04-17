ba0587042c83a3df4c38f1d0883ed2fe14117eda
sunchangqing001 <759061715@qq.com>
2019-04-17 11:07:39 +0800

    修改HelloGit内容

--- src/com/sun/test/HelloGit.java
+++ src/com/sun/test/HelloGit.java
@@ -9,11 +9,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 0;
-		while(i<5) {
-			System.out.println(i);
-			i++;
-		}
+		System.out.println("ÕâÊÇHelloGit£¡");
 	}
 
 }
